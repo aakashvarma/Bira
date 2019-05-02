427416a34646cca5c19e5212f3f632fe820bfb04
Manmeet Tarun <meetutarun16@gmail.com>
2019-05-02 17:48:47 +0530

    Terminal Changes

--- out/extension.js
+++ out/extension.js
@@ -8,14 +8,21 @@ const vscode = require("vscode");
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
+    console.log('Congratulations, your extension "bira" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.hallo', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello Manmeet');
+        vscode.workspace.saveAll();
+        let terminal = vscode.window.createTerminal('coderunner');
+        terminal.show();
+        let name = terminal.name;
+        terminal.processId.then((val) => {
+            console.log(`Terminal opened with ${val} id`);
+        });
+        vscode.window.showInformationMessage('Processing.....');
     });
     context.subscriptions.push(disposable);
 }
